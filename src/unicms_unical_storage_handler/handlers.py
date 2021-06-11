0eb4b814b0c21ddded1863b2b585410097092c30
Francesco Filicetti <francesco.filicetti@unical.it>
2021-06-11 12:42:37 +0200

    feat: block templates

--- src/unicms_unical_storage_handler/handlers.py
+++ src/unicms_unical_storage_handler/handlers.py
@@ -12,64 +12,76 @@ from cms.pages.models import Page
 from . settings import *
 
 
-class CdSListInfoViewHandler(BaseContentHandler):
-    template = "storage_cdslist.html"
+class BaseStorageHandler(BaseContentHandler):
+    template = "example.html"
 
     def __init__(self, **kwargs):
-        super(CdSListInfoViewHandler, self).__init__(**kwargs)
+        super(BaseStorageHandler, self).__init__(**kwargs)
         self.match_dict = self.match.groupdict()
         self.webpath = WebPath.objects.filter(site=self.website,
                                               parent=None,
                                               is_active=True)\
                                       .first()
-        self.code = self.match_dict.get('code', '')
-
         if self.webpath.site.pk not in settings.ALLOWED_UNICMS_SITES:
             raise Http404('Website not allowed to show this webpath')
 
+        # only home page of allowed websites
         if not self.webpath or self.match_dict.get('webpath', '/') != self.webpath.fullpath: # pragma: no cover
             raise Http404('Unknown WebPath')
 
         self.page = Page.objects.filter(is_active=True,
                                         webpath=self.webpath).first()
-
-    def as_view(self):
-        # if not self.pub_context: return Http404()
-
-        # i18n
-        # lang = getattr(self.request, 'LANGUAGE_CODE', None)
-        # if lang:
-            # self.pub_context.publication.translate_as(lang=lang)
-
-        data = {'request': self.request,
-                'webpath': self.webpath,
-                'website': self.website,
-                'page': self.page,
-                'path': self.match_dict.get('webpath', '/'),
-                'handler': self,
-                'url': f'{settings.CMS_STORAGE_CDSINFO_API}/{self.code}/'
+        self.data = {'request': self.request,
+                     'webpath': self.webpath,
+                     'website': self.website,
+                     'page': self.page,
+                     'path': self.match_dict.get('webpath', '/'),
+                     'handler': self
         }
 
+    def as_view(self):
         ext_template_sources = contextualize_template(self.template,
                                                       self.page)
         template = Template(ext_template_sources)
-        context = Context(data)
+        context = Context(self.data)
         return HttpResponse(template.render(context), status=200)
 
+
+class CdSListViewHandler(BaseStorageHandler):
+    template = "storage_cdslist.html"
+
+    def as_view(self):
+        self.data['url'] = f'{settings.CMS_STORAGE_CDS_API}/'
+        return super().as_view()
+
     @property
-    def parent_path_prefix(self):
-        return ''
-        return getattr(settings, 'CMS_PUBLICATION_LIST_PREFIX_PATH',
-                       CMS_PUBLICATION_LIST_PREFIX_PATH)
+    def breadcrumbs(self):
+        # leaf = (self.pub_context.url, getattr(self.pub_context.publication, 'title'))
+        leaf = ('#', settings.CMS_STORAGE_CDSLIST_LABEL)
+        parent = ('#', settings.CMS_STORAGE_ROOT_LABEL)
+        return (parent, leaf)
+
+
+class CdSInfoViewHandler(BaseStorageHandler):
+    template = "storage_cdsinfo.html"
+
+    def __init__(self, **kwargs):
+        super(CdSInfoViewHandler, self).__init__(**kwargs)
+        self.code = self.match_dict.get('code', '')
+
+    def as_view(self):
+        self.data['url'] = f'{settings.CMS_STORAGE_CDS_API}/{self.code}/'
+        return super().as_view()
 
     @property
     def parent_url(self):
-        url = f'{self.webpath.get_full_path()}/{self.parent_path_prefix}/'
+        url = f'{self.webpath.get_full_path()}/{settings.CMS_STORAGE_CDS_VIEW_PREFIX_PATH}/'
         return sanitize_path(url)
 
     @property
     def breadcrumbs(self):
         # leaf = (self.pub_context.url, getattr(self.pub_context.publication, 'title'))
-        leaf = ('#', 'ahahh')
-        parent = (self.parent_url, _('News'))
-        return (parent, leaf)
+        root = ('#', settings.CMS_STORAGE_ROOT_LABEL)
+        parent = (self.parent_url, settings.CMS_STORAGE_CDSLIST_LABEL)
+        leaf = ('#', self.code)
+        return (root, parent, leaf)
--- src/unicms_unical_storage_handler/settings.py
+++ src/unicms_unical_storage_handler/settings.py
@@ -1,5 +1,7 @@
 import logging
 
+from django.utils.translation import gettext_lazy as _
+
 
 logger = logging.getLogger(__name__)
 
@@ -11,22 +13,29 @@ ALLOWED_UNICMS_SITES = [2]
 CMS_STORAGE_BASE_PATH = 'storage/'
 
 # as documentation reference or default
-CMS_STORAGE_CDSLIST_VIEW_PREFIX_PATH = f'{CMS_STORAGE_BASE_PATH}cdslist/'
-CMS_STORAGE_CDSLIST_URL_VIEW_REGEXP = f'^(?P<webpath>[\/a-zA-Z0-9\.\-\_]*)({CMS_STORAGE_CDSLIST_VIEW_PREFIX_PATH})(?P<code>[a-z0-9\-]*)' # noqa
+CMS_STORAGE_CDS_VIEW_PREFIX_PATH = f'{CMS_STORAGE_BASE_PATH}cds'
+CMS_STORAGE_CDSLIST_URL_VIEW_REGEXP = f'^(?P<webpath>[\/a-zA-Z0-9\.\-\_]*)({CMS_STORAGE_CDS_VIEW_PREFIX_PATH})(/)?$' # noqa
+CMS_STORAGE_CDSINFO_URL_VIEW_REGEXP = f'^(?P<webpath>[\/a-zA-Z0-9\.\-\_]*)({CMS_STORAGE_CDS_VIEW_PREFIX_PATH})/(?P<code>[a-z0-9\-]*)(/)?$' # noqa
 
-CMS_STORAGE_STUDIACTIVITYINFO_VIEW_PREFIX_PATH = f'{CMS_STORAGE_BASE_PATH}/studyactivityinfo/'
-CMS_STORAGE_STUDIACTIVITYINFO_URL_VIEW_REGEXP = f'^(?P<webpath>[\/a-zA-Z0-9\.\-\_]*)({CMS_STORAGE_STUDIACTIVITYINFO_VIEW_PREFIX_PATH})(?P<code>[a-z0-9\-]*)' # noqa
+CMS_STORAGE_STUDIACTIVITYINFO_VIEW_PREFIX_PATH = f'{CMS_STORAGE_BASE_PATH}/studyactivityinfo'
+CMS_STORAGE_STUDIACTIVITYINFO_URL_VIEW_REGEXP = f'^(?P<webpath>[\/a-zA-Z0-9\.\-\_]*)({CMS_STORAGE_STUDIACTIVITYINFO_VIEW_PREFIX_PATH})(?P<code>[a-z0-9\-]*)(/)?' # noqa
 
-CMS_STORAGE_HANDLERS_PATHS = [CMS_STORAGE_CDSLIST_VIEW_PREFIX_PATH,
-                              CMS_STORAGE_STUDIACTIVITYINFO_VIEW_PREFIX_PATH]
+CMS_STORAGE_HANDLERS_PATHS = [CMS_STORAGE_CDSLIST_URL_VIEW_REGEXP,
+                              CMS_STORAGE_CDSINFO_URL_VIEW_REGEXP,
+                              CMS_STORAGE_STUDIACTIVITYINFO_URL_VIEW_REGEXP]
 
 CMS_STORAGE_APP_REGEXP_URLPATHS = {
     # 'cms_storage.handlers.StudyActivityInfoViewHandler' : CMS_STORAGE_STUDIACTIVITYINFO_URL_VIEW_REGEXP,
-    'unicms_unical_storage_handler.handlers.CdSListInfoViewHandler' : CMS_STORAGE_CDSLIST_URL_VIEW_REGEXP,
+    'unicms_unical_storage_handler.handlers.CdSListViewHandler' : CMS_STORAGE_CDSLIST_URL_VIEW_REGEXP,
+    'unicms_unical_storage_handler.handlers.CdSInfoViewHandler' : CMS_STORAGE_CDSINFO_URL_VIEW_REGEXP,
 }
 
 # cms_storage APIs (ref: https://storage.unical.it)
 CMS_STORAGE_BASE_API = 'https://storage.portale.unical.it/api/ricerca/'
-CMS_STORAGE_CDSLIST_API = f'{CMS_STORAGE_BASE_API}cdslist/'
+
+CMS_STORAGE_CDS_API = f'{CMS_STORAGE_BASE_API}cds/'
 CMS_STORAGE_STUDIACTIVITYINFO_API = f'{CMS_STORAGE_BASE_API}studiactivityinfo/'
-CMS_STORAGE_CDSINFO_API = f'{CMS_STORAGE_BASE_API}cds/'
+
+# labels (for breadcrumbs and page title)
+CMS_STORAGE_ROOT_LABEL = _("Data storage")
+CMS_STORAGE_CDSLIST_LABEL = _("Corsi di studio")
