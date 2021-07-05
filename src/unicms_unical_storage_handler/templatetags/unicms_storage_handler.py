28e4959cc84f9386bc4f9171d418a7b6b6e09a01
Francesco Filicetti <francesco.filicetti@unical.it>
2021-07-05 08:42:18 +0200

    fix: templatetag removed

--- src/unicms_unical_storage_handler/templatetags/unicms_storage_handler.py
+++ src/unicms_unical_storage_handler/templatetags/unicms_storage_handler.py
@@ -1,6 +1,4 @@
 import logging
-import random
-import string
 
 from django import template
 
@@ -8,12 +6,6 @@ from django import template
 logger = logging.getLogger(__name__)
 register = template.Library()
 
-
-@register.simple_tag
-def random_id():
-    # printing lowercase
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(10))
 
 @register.simple_tag
 def clean_url(url):
